32b8ff2397a76bf0c7ab08e21b69713045a4a5d1
Bruno <bruno.mma97@gmail.com>
2024-12-10 19:28:02 +0000

    Day 10 part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -14,14 +14,6 @@ fn test_input_1() -> &'static str {
 
 fn test_input_2() -> &'static str {
 	concat!(
-		"0123\n",
-		"1234\n",
-		"8765\n",
-		"9876\n"
-	)
-}
-fn test_input_3() -> &'static str {
-	concat!(
 		"89010123\n",
 		"78121874\n",
 		"87430965\n",
@@ -58,12 +50,12 @@ fn sum_trailhead_scores(map_size: (i32, i32), map: &[u8]) -> usize {
 	(0..map_size.0)
 		.flat_map(|row| (0.. map_size.1).map(move |column| (row, column)))
 		.filter(|position| get_map_height(*position, map_size, map) == 0)
-		.map(|trailhead_start| get_trailhead_score(trailhead_start, 0, map_size, map))
+		.map(|trailhead_start| get_trailhead_end_positions(trailhead_start, 0, map_size, map))
 		.map(|trailhead_ends| trailhead_ends.len())
 		.sum()
 }
 
-fn get_trailhead_score(curr_pos: (i32, i32), current_height: u8, map_size: (i32, i32), map: &[u8]) -> HashSet<(i32, i32)> {
+fn get_trailhead_end_positions(curr_pos: (i32, i32), current_height: u8, map_size: (i32, i32), map: &[u8]) -> HashSet<(i32, i32)> {
 	let mut result = HashSet::new();
 	if curr_pos.0 < 0 || curr_pos.0 >= map_size.0 || curr_pos.1 < 0 || curr_pos.1 >= map_size.1 {
 		return result;
@@ -77,8 +69,32 @@ fn get_trailhead_score(curr_pos: (i32, i32), current_height: u8, map_size: (i32,
 	let new_height = current_height + 1;
 	[(0,1), (1, 0), (0, -1), (-1, 0)].iter()
 		.map(|pos_delta| (pos_delta.0 + curr_pos.0, pos_delta.1 + curr_pos.1))
-		.flat_map(|new_pos| get_trailhead_score(new_pos, new_height, map_size, map))
+		.flat_map(|new_pos| get_trailhead_end_positions(new_pos, new_height, map_size, map))
 		.collect()
+}
+
+fn sum_trailhead_ratings(map_size: (i32, i32), map: &[u8]) -> usize {
+	(0..map_size.0)
+		.flat_map(|row| (0.. map_size.1).map(move |column| (row, column)))
+		.filter(|position| get_map_height(*position, map_size, map) == 0)
+		.map(|trailhead_start| get_trailhead_rating(trailhead_start, 0, map_size, map))
+		.sum()
+}
+
+fn get_trailhead_rating(curr_pos: (i32, i32), current_height: u8, map_size: (i32, i32), map: &[u8]) -> usize {
+	if curr_pos.0 < 0 || curr_pos.0 >= map_size.0 || curr_pos.1 < 0 || curr_pos.1 >= map_size.1 {
+		return 0;
+	}
+	if get_map_height(curr_pos, map_size, map) != current_height {return 0}
+	if current_height == 9 {
+		return 1;
+	}
+
+	let new_height = current_height + 1;
+	[(0,1), (1, 0), (0, -1), (-1, 0)].iter()
+		.map(|pos_delta| (pos_delta.0 + curr_pos.0, pos_delta.1 + curr_pos.1))
+		.map(|new_pos| get_trailhead_rating(new_pos, new_height, map_size, map))
+		.sum()
 }
 
 fn main() {
@@ -87,4 +103,5 @@ fn main() {
 	let (map_size, map) = parse_input(input);
 
 	println!("Trailhead scores: {}", sum_trailhead_scores(map_size, &map));
+	println!("Trailhead ratings: {}", sum_trailhead_ratings(map_size, &map));
 }
