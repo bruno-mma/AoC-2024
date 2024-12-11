6143f994e4aa4e44da3f7ef0a04ebc41fb48052c
Bruno <bruno.mma97@gmail.com>
2024-12-11 23:33:13 +0000

    Day 11 part 2 (much faster)

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
 use std::fs;
 
 fn test_input_1() -> &'static str {
@@ -14,15 +15,28 @@ fn read_input_file(file_name: &str) -> String {
 	fs::read_to_string(file_name).unwrap()
 }
 
-fn parse_input(input: &str) -> Vec<u64> {
-	input.split(" ")
-		.map(|str| str.trim_end())
+fn parse_input(input: &str) -> HashMap<u64, u64> {
+	input
+		.split_whitespace()
 		.map(|num| num.parse().unwrap())
-		.collect()
+		.fold(HashMap::new(), |mut stone_counts, stone| {
+			*stone_counts.entry(stone).or_insert(0) += 1;
+			stone_counts
+		})
+}
+
+
+fn digit_count(mut num: u64) -> u32 {
+	let mut digit_count = 0;
+	while num != 0 {
+		num /= 10;
+		digit_count += 1;
+	}
+	digit_count
 }
 
 fn split_if_digit_count_even(num: u64) -> Option<(u64, u64)> {
-	let digit_count = num.ilog10() + 1;
+	let digit_count = digit_count(num);
 	if digit_count % 2 == 0 {
 		let mask = 10_u64.pow(digit_count / 2);
 		let right_split = num % mask;
@@ -33,24 +47,34 @@ fn split_if_digit_count_even(num: u64) -> Option<(u64, u64)> {
 	}
 }
 
-fn blink_times(mut stones: Vec<u64>, blink_count: u32) -> Vec<u64> {
-	for blink in 0..blink_count {
-		// println!("Blink {}", blink);
-		let mut i = 0;
-		while i < stones.len() {
-			if stones[i] == 0 {
-				stones[i] = 1;
-			}
-			else if let Some(stone_split) = split_if_digit_count_even(stones[i]) {
-				stones[i] = stone_split.0;
-				stones.insert(i + 1, stone_split.1);
-				i += 1;
-			} else {
-				stones[i] *= 2024;
-			}
-			i += 1;
-		}
+fn get_stone_blink_result(stone: u64) -> (u64, Option<u64>) {
+	if stone == 0 {
+		return (1, None);
 	}
+
+	if let Some(stone_split) = split_if_digit_count_even(stone) {
+		(stone_split.0, Some(stone_split.1))
+	} else {
+		(stone * 2024, None)
+	}
+}
+
+fn blink_times(mut stones: HashMap<u64, u64>, blink_count: u32) -> HashMap<u64, u64> {
+	for _ in 0..blink_count {
+		let mut blink_result = HashMap::new();
+
+		for (stone, stone_count) in stones.iter() {
+			let new_stones = get_stone_blink_result(*stone);
+
+			*blink_result.entry(new_stones.0).or_insert(0) += stone_count;
+			if let Some(new_stone_1) = new_stones.1 {
+				*blink_result.entry(new_stone_1).or_insert(0) += stone_count;
+			}
+		}
+
+		stones = blink_result.clone();
+	}
+
 	stones
 }
 
@@ -58,8 +82,9 @@ fn main() {
 	// let input = test_input_1();
 	// let input = test_input_2();
 	let input = &read_input_file("input.txt");
-	
-	let stones = parse_input(input);
-	println!("Number of stones after 25 blinks: {:?}", blink_times(stones.clone(), 25).len());
-	// println!("Number of stones after 75 blinks: {:?}", blink_times(stones, 75).len());
+
+	let stone_to_stone_count = parse_input(input);
+
+	println!("Number of stones after 25 blinks: {}", blink_times(stone_to_stone_count.clone(), 25).values().sum::<u64>());
+	println!("Number of stones after 75 blinks: {}", blink_times(stone_to_stone_count, 75).values().sum::<u64>());
 }
